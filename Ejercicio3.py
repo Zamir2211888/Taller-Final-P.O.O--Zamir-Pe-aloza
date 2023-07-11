389e1a10675aa18439775f7334cb256ee86fecc7
Zamir Peñaloza <zamir2211888@correo.uis.edu.co>
2023-07-11 09:33:27 -0500

    Update Ejercicio3.py

---
+++ Ejercicio3.py
@@ -0,0 +1,15 @@
+#Ejercicio 3 Zamir Peñaloza 
+
+
+class Vuelo:
+    def __init__(self) -> None:
+        pass
+
+
+class Pasajero:
+    def __init__(self) -> None:
+        pass
+    
+class Reserva:
+    def __init__(self) -> None:
+        pass
